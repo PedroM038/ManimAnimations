f4a92abdea00a8c7599f9cb70723bd9b272d977f
Pedro Marques <pedrohmarques640@gmail.com>
2024-08-30 01:57:44 -0300

    added Derivatives animations. Completed Integrals animation. Missing comments and modularity in the Derivatives code.

--- Of1/Derivative.py
+++ Of1/Derivative.py
@@ -1,36 +1,129 @@
-from manim import *
+from manim import (
+    BLUE, DOWN, GREEN_B, RED, RIGHT, UR, WHITE, Axes, Create, Dot,
+    DrawBorderThenFill, FadeOut, LaggedStart, Line, MathTex,
+    NumberPlane, Scene, Transform, ValueTracker, VGroup,
+    Write, always_redraw, linear, smooth
+)
 
 class DerivativeDemo(Scene):
     def construct(self):
+        # Creating the coordinate plane
         plane = NumberPlane(x_range=[-3, 3], y_range=[-4, 14], y_length=7, x_length=6)
 
+        # Creating the quadratic function
         graph1 = plane.plot(lambda x: x**2, x_range=[-3, 3], color=RED)
         graph1_lab = (
-            MathTex("f(x) = x^2")
-            .next_to(graph1, UR, buff=0.2)
-            .set_color(RED)
-            .scale(0.8)
+            MathTex("f(x) = x^2").next_to(graph1, UR, buff=1).set_color(RED).scale(0.8)
         )
 
+        # Creating the ValueTracker for the secant line animation
         k = ValueTracker(-3)
 
+        # Points of the secant line
         dot1 = always_redraw(
             lambda: Dot().move_to(plane.c2p(k.get_value(), k.get_value() ** 2))
         )
+
+        # Secant line and its labels
         slope1 = always_redraw(
             lambda: plane.get_secant_slope_group(
-                x=k.get_value(), graph=graph1, dx=0.01, secant_line_length=5
+                x=k.get_value(),
+                graph=graph1,
+                dx=0.01,
+                dx_label=MathTex(r"\Delta x"),
+                dy_label=MathTex(r"\Delta y"),
+                dx_line_color=GREEN_B,
+                secant_line_length=4,
+                secant_line_color=BLUE,
             )
         )
 
+        slope_lab = always_redraw(
+            lambda: MathTex("r")
+            .next_to(slope1.secant_line, RIGHT, buff=0.2)
+            .set_color(BLUE)
+            .scale(0.8)
+        )
+
+        # Initial animation: Drawing the function and the secant line
         self.play(
-            LaggedStart(DrawBorderThenFill(plane), Create(graph1)),
+            LaggedStart(DrawBorderThenFill(plane), Create(graph1), Create(graph1_lab)),
             run_time=8,
             lag_ratio=1,
         )
-        self.add(slope1, dot1, graph1_lab)
+        self.add(slope1, slope_lab, dot1)
 
-        self.play(k.animate.set_value(0), run_time=8, rate_func=linear)
-        self.play(k.animate.set_value(3), run_time=8, rate_func=linear)
+        self.play(k.animate.set_value(0), run_time=8, rate_func=smooth)
+        self.play(k.animate.set_value(3), run_time=8, rate_func=smooth)
 
         self.wait()
+
+        self.play(k.animate.set_value(0), run_time=8, rate_func=linear)
+
+        # FadeOut to clear the screen
+        self.play(
+            FadeOut(VGroup(plane, graph1, graph1_lab, dot1, slope_lab, slope1)),
+            run_time=2,
+        )
+
+        # Creating a new line at the top of the screen
+        line_r = Line(start=plane.c2p(-3, 10), end=plane.c2p(3, 10), color=BLUE)
+        line_r_label = (
+            MathTex(r"r = mx + b").next_to(line_r, DOWN, buff=0.5).set_color(BLUE)
+        )
+
+        self.play(Create(line_r), Write(line_r_label))
+        self.wait(3)
+
+        # Showing the slope formula (m)
+        delta_formula = (
+            MathTex(r"m = \frac{\Delta y}{\Delta x}")
+            .next_to(line_r_label, DOWN, buff=0.5)
+            .set_color(WHITE)
+        )
+        self.play(Write(delta_formula))
+        self.wait(3)
+
+        # Transforming into the general slope formula
+        point_formula = (
+            MathTex(r"m = \frac{y - y_0}{x - x_0}")
+            .next_to(delta_formula, DOWN, buff=0.5)
+            .set_color(WHITE)
+        )
+        self.play(Transform(delta_formula, point_formula))
+        self.wait(3)
+
+        # FadeOut of the line and formulas
+        self.play(FadeOut(VGroup(line_r, delta_formula, point_formula, line_r_label)))
+
+        axes = Axes(y_range=[-1, 12])
+        graph2 = axes.plot(lambda x: 1 / 4 * x * x, color=BLUE)
+        graph2_lab = (
+            MathTex("f(x) = x^2").next_to(graph2, UR, buff=1).set_color(BLUE).scale(0.8)
+        )
+
+        # Recreating the quadratic function
+        self.play(DrawBorderThenFill(axes), Create(graph2), Create(graph2_lab))
+
+        k = ValueTracker(3)
+        d = ValueTracker(3)
+
+        slope2 = always_redraw(
+            lambda: axes.get_secant_slope_group(
+                x=k.get_value(),
+                graph=graph2,
+                dx=d.get_value(),
+                dx_label=MathTex(r"\Delta x"),
+                dy_label=MathTex(r"\Delta y"),
+                dx_line_color=GREEN_B,
+                secant_line_length=4,
+                secant_line_color=RED,
+            )
+        )
+
+        self.add(slope2)
+        self.play(k.animate.set_value(0), run_time=12, rate_func=smooth)
+        self.play(d.animate.set_value(0.1), run_time=8, rate_func=smooth)
+        self.play(k.animate.set_value(-3), run_time=4, rate_func=smooth)
+        self.wait(2)
+
--- Of1/Integral.py
+++ Of1/Integral.py
@@ -1,38 +1,64 @@
-from manim import (PURPLE, UR, WHITE, Axes, Create, DrawBorderThenFill,
-                   LaggedStart, MathTex, Scene, Tex, Transform, VGroup)
+from manim import (
+    PURPLE,
+    UR,
+    WHITE,
+    Axes,
+    Create,
+    DrawBorderThenFill,
+    LaggedStart,
+    MathTex,
+    Scene,
+    Tex,
+    Transform,
+    VGroup,
+)
 
 
 class IntegralDemo(Scene):
-    def integral_func_1(self):
-        axes = Axes(x_range=[-5, 5], x_length=8, y_range=[-10, 10], y_length=7)
+    def create_graph_with_rectangles(
+        self, axes, func, func_label, graph_color, x_range, y_range, dx_list
+    ):
+        """
+        Create a graph of a function and animate the approximation of the integral
+        using Riemann rectangles with decreasing dx values.
 
-        label_axes = axes.get_axis_labels(Tex("x").scale(1), Tex("y").scale(1))
+        Parameters:
+        axes (Axes): The axes on which to plot the graph.
+        func (function): The mathematical function to plot.
+        func_label (str): The label for the function.
+        graph_color (color): The color of the graph.
+        x_range (list): The x-range for the plot.
+        y_range (list): The y-range for the plot.
+        dx_list (list): List of dx values for the Riemann rectangles.
+        """
+        # Create the graph
+        graph = axes.plot(func, x_range=x_range, color=graph_color)
 
-        graph = axes.plot(
-            lambda x: 0.1 * (x - 4) * (x - 1) * (x + 3), x_range=[-5, 5], color=PURPLE
-        )
-
+        # Create the label for the graph
         graph_lab = (
-            MathTex("y = f(x)").next_to(graph, UR, buff=0.5).set_color(PURPLE).scale(1)
+            MathTex(func_label)
+            .next_to(graph, UR, buff=0.5)
+            .set_color(graph_color)
+            .scale(1)
         )
 
+        # Animate the creation of axes, labels, and graph
+        label_axes = axes.get_axis_labels(Tex("x").scale(1), Tex("y").scale(1))
         self.play(
             LaggedStart(DrawBorderThenFill(axes)),
             Create(label_axes),
             run_time=5,
             lag_ratio=1,
         )
-
         self.play(Create(graph), Create(graph_lab), run_time=3)
-
         self.wait(1)
 
-        dx_list = [1, 0.5, 0.3, 0.1, 0.05, 0.025, 0.01, 0.005]
+        # Create Riemann rectangles for different dx values
         rectangles = VGroup(
             *[
                 axes.get_riemann_rectangles(
                     graph=graph,
-                    x_range=[-5, 5],
+                    x_range=x_range,
                     stroke_width=0.1,
                     stroke_color=WHITE,
                     dx=dx,
@@ -40,56 +66,10 @@ class IntegralDemo(Scene):
                 for dx in dx_list
             ]
         )
+
+        # Animate the transformation of the rectangles
         first_area = rectangles[0]
         self.play(Create(first_area), run_time=2)
-
-        for k in range(1, len(dx_list)):
-            new_area = rectangles[k]
-            self.play(Transform(first_area, new_area), run_time=3)
-            self.wait(2)
-
-        self.wait()
-
-    def integral_func_2(self):
-        axes = Axes(x_range=[-5, 5], x_length=8, y_range=[-5, 15], y_length=7)
-
-        label_axes = axes.get_axis_labels(Tex("x").scale(1), Tex("y").scale(1))
-
-        graph = axes.plot(
-            lambda x: 0.5 * x * x, x_range=[-5, 5], color=PURPLE
-        )
-
-        graph_lab = (
-            MathTex("y = x^2").next_to(graph, UR, buff=0.5).set_color(PURPLE).scale(1)
-        )
-
-        self.play(
-            LaggedStart(DrawBorderThenFill(axes)),
-            Create(label_axes),
-            run_time=5,
-            lag_ratio=1,
-        )
-
-        self.play(Create(graph), Create(graph_lab), run_time=3)
-
-        self.wait(1)
-
-        dx_list = [1, 0.5, 0.3, 0.1, 0.05, 0.025, 0.01, 0.005]
-        rectangles = VGroup(
-            *[
-                axes.get_riemann_rectangles(
-                    graph=graph,
-                    x_range=[-5, 5],
-                    stroke_width=0.1,
-                    stroke_color=WHITE,
-                    dx=dx,
-                )
-                for dx in dx_list
-            ]
-        )
-        first_area = rectangles[0]
-        self.play(Create(first_area), run_time=2)
-
         for k in range(1, len(dx_list)):
             new_area = rectangles[k]
             self.play(Transform(first_area, new_area), run_time=3)
@@ -98,9 +78,37 @@ class IntegralDemo(Scene):
         self.wait()
 
     def construct(self):
-        self.integral_func_1()
+        # Set up the axes and dx_list for both functions
+        axes1 = Axes(x_range=[-5, 5], x_length=8, y_range=[-10, 10], y_length=7)
+        axes2 = Axes(x_range=[-5, 5], x_length=8, y_range=[-5, 15], y_length=7)
+        dx_list = [1, 0.5, 0.3, 0.1, 0.05, 0.025, 0.01, 0.005]
+
+        # Function 1: f(x) = 0.1 * (x - 4) * (x - 1) * (x + 3)
+        self.create_graph_with_rectangles(
+            axes=axes1,
+            func=lambda x: 0.1 * (x - 4) * (x - 1) * (x + 3),
+            func_label="y = f(x)",
+            graph_color=PURPLE,
+            x_range=[-5, 5],
+            y_range=[-10, 10],
+            dx_list=dx_list,
+        )
+
+        # Clear the scene before the next graph
         self.clear()
         self.wait()
-        self.integral_func_2()
+
+        # Function 2: y = 0.5 * x^2
+        self.create_graph_with_rectangles(
+            axes=axes2,
+            func=lambda x: 0.5 * x * x,
+            func_label="y = x^2",
+            graph_color=PURPLE,
+            x_range=[-5, 5],
+            y_range=[-5, 15],
+            dx_list=dx_list,
+        )
+
+        # Clear the scene at the end
         self.clear()
         self.wait()
